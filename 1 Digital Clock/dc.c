021ca1de5f1c43744eb038ee3d0d90242c810968
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-29 12:49:44 +0530

    build and completed project

--- 1 Digital Clock/dc.c
+++ 1 Digital Clock/dc.c
@@ -4,11 +4,11 @@
 #include<stdlib.h>
 
 int main(){
-  {
+  
     int hour,minute,second;
     hour = minute=second=0;
     while(1){
-      system("clear");
+      system("cls");
       printf("%02d : %02d : %2d",hour,minute,second);
       fflush(stdout);
       second++;
@@ -17,7 +17,20 @@ int main(){
         minute+=1;
         second=0;
       }
+      if(minute==60)
+      {
+        hour+=1;
+        minute=0;
+      }
+      if(hour == 24)
+      {
+        hour=0;
+        minute=0;
+        second=0;
+      }
+      sleep(1);
     }
-  }
-  return 0;
+    return 0;
+
+
 }
