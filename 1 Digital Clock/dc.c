f12b717999b60b7f4460dec975fed33d6cfe45fa
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-29 12:41:19 +0530

    adding some library for project

--- 1 Digital Clock/dc.c
+++ 1 Digital Clock/dc.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
+#include<time.h>
+#include<unistd.h>
+#include<stdlib.h>
 
 int main(){
+  {
+    int hour,minute,second;
+    hour = minute=second=0;
+    while(1){
+      system("clear");
+      printf("%02d : %02d : %2d",hour,minute,second);
+      fflush(stdout);
+      second++;
+      if(second==60)
+      {
+        minute+=1;
+        second=0;
+      }
+    }
+  }
   return 0;
 }
